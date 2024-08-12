7d377ce3307d646a0f9ad4c537c695219f09c6c5
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2024-08-12 11:29:29 +0100

    Update and rename Get_risk_models.py to get_risk_models.py

--- 1.Built_In_Functions/get_risk_models.py
+++ 1.Built_In_Functions/get_risk_models.py
@@ -1,10 +1,10 @@
 #############################################################################################################################################################################################
 #
 # get_risk_models_for_identifier(identifier)
-# is a QI API endpoint to retrieve risk models for a specific identifier.
+# is a QI API endpoint to retrieve all risk models available for a specific identifier.
 # 
 # Inputs:
-#               * identifier[str] (required) - The identifier you want risk models for.
+#               * identifier[str] (required) - The identifier you want risk models for. Identifier can be a SEDOL, ISIN or Bloomberg Ticker.
 # Output: List
 #               * e.g.
 #                    ['QI_US_MACRO_MT_1',
@@ -28,10 +28,12 @@ configuration.api_key['X-API-KEY'] = 'YOUR_API_KEY'
 # Instantiate API class.
 api_instance = qi_client.DefaultApi(qi_client.ApiClient(configuration))
 
+identifier = 'AAPL US Equity'
+
 try:
-    api_response = api_instance.get_risk_models()
+    api_response = api_instance.get_risk_models_for_identifier(identifier)
     pprint(api_response)
 
 except ApiException as e:
-    print(f"Exception when calling DefaultApi:get_risk_models: {e}")
+    print(f"Exception when calling DefaultApi:get_risk_models_for_identifier: {e}")
 
