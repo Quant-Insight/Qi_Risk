5130c3203c2f33e13009d34828f85c84a2d439e5
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2024-08-12 11:24:30 +0100

    Update Get_risk_model_data_for_risk_model.py

--- 1.Built_In_Functions/Get_risk_model_data_for_risk_model.py
+++ 1.Built_In_Functions/Get_risk_model_data_for_risk_model.py
@@ -1,13 +1,18 @@
 #############################################################################################################################################################################################
 
-# get_risk_model_data_for_risk_model(risk_model, instrument, date_from=date_from,date_to=date_to)
-# is a QI API endpoint to retrieve risk model data for a specific instrument during a given time frame.
+# get_risk_model_data_for_risk_model(risk_model, instrument/identifier, date_from=date_from, date_to=date_to)
+# is a QI API endpoint to retrieve risk model data for a specific instrument or identifier during a given time frame (max 1 year per query).
 # 
 # Inputs:
 #               * date_from [date] (optional) - End date of data required 'YYYY-MM-DD' format.
 #               * date_to [date] (optional) - End date of data required 'YYYY-MM-DD' format.
-#               * risk_model[str] (required) - The Factor Set. If your equity focus consists of approximately 90% US or Canadian assets, please use the US Models with or without the market (QI_US_MACRO_MT_1). All securities have Global and US models.
-#               * instrument[str] (required) - The instrument that you want to pull data from e.g 'SPY'
+#               * risk_model[str] (required) - 8 models (4 regions with and without market): QI_US_MACROMKT_MT_1, QI_US_MACRO_MT_1, QI_EU_MACROMKT_MT_1, QI_EU_MACRO_MT_1,
+#                                                                                            QI_APAC_MACROMKT_MT_1, QI_APAC_MACRO_MT_1, QI_GLOBAL_MACROMKT_MT_1, QI_GLOBAL_MACRO_MT_1.
+#               * instrument[str] or identifier[str] (required) - The instrument or identifier you want to pull data for. For instrument use Qi's default asset naming convention, e.g. 'AAPL'.
+#                                                                 For identifier use asset's SEDOL, ISIN or Bloomberg Ticker.
+#
+# If your equity focus consists of approximately 90% US or Canadian assets, please use the US Model with or without the market. All securities have Global and US models.
+#
 # Output: Dictionary
 # Units: %
 #               * e.g.
